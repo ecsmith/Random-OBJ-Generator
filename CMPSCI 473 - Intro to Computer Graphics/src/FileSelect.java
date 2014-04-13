a362595cf38b322dace6ad9a83732edf0dca922e
Eric Smith <ecsmith@student.umass.edu>
2014-04-13 14:47:30 -0400

    File Reading Done

--- CMPSCI 473 - Intro to Computer Graphics/src/FileSelect.java
+++ CMPSCI 473 - Intro to Computer Graphics/src/FileSelect.java
@@ -15,11 +15,22 @@ class FileSelect
 
             if (returnVal == JFileChooser.APPROVE_OPTION) {
                 File file = fc.getSelectedFile();
+
                 //This is where a real application would open the file.
                 System.out.println("Opening: " + file.getName() + ".\n");
+                System.out.println("Size in Bytes: " + file.getFreeSpace() + ".\n");
+                try{
+                    RandomAccessFile FILE = new RandomAccessFile(file.getAbsoluteFile(), "rw");
+                    System.out.println("Size in Bytes: " + FILE.length() + ".\n");
+                    FILE.close(); 
+                   } catch (IOException e) {//If the file is not found
+           			e.printStackTrace();  //Print trace the error
+           		}
+
             } else {
                 System.out.println("Open command cancelled by user.\n");
             }
+        
         }
     });
    }
